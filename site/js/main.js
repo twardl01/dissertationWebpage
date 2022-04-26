04d2741bc93b9e93081857ed701f319bcf6c7581
twardl01 <81767595+twardl01@users.noreply.github.com>
2022-04-26 10:23:16 +0100

    addition of jquery to library, addition to main.js

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,17 @@
 var game;
 
-requirejs(["TicTacToeGame", "TicTacToeEngine", "chatbotPlayer", "humanPlayer", "lib/tmi.js"], function() {
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: 'lib',
+    paths: {
+        app: '../js',
+        jquery: 'jquery-3.6.0'
+    }
+});
+
+requirejs(["app/TicTacToeGame", "app/TicTacToeEngine", "app/chatbotPlayer", "app/humanPlayer", "lib/tmi.js", "jquery"], function() {
 
     game = new TicTacToeGame();
     game.start();
